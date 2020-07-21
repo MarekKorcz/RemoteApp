b087aacbe61b2384b505992338d7850b887ca8d7
MarkKorcz <mark.korcz@gmail.com>
2020-07-21 08:20:57 +0200

    cleaning folder

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const { exec } = require("child_process")
+// const { exec } = require("child_process")
 
-// exec("touch new.js; rm -rf new.js;")
+// // exec("touch new.js; rm -rf new.js;")
 
-// exec("ls -la", (error, stdout, stderr) => {
+// // exec("ls -la", (error, stdout, stderr) => {
+// //     if (error) {
+// //         console.log(`error: ${error.message}`);
+// //         return;
+// //     }
+// //     if (stderr) {
+// //         console.log(`stderr: ${stderr}`);
+// //         return;
+// //     }
+// //     console.log(stdout);
+// // })
+
+// let commandLine = "node grazyna.js"
+
+// exec(commandLine, (error, stdout, stderr) => {
 //     if (error) {
 //         console.log(`error: ${error.message}`);
 //         return;
@@ -11,19 +25,6 @@ const { exec } = require("child_process")
 //         console.log(`stderr: ${stderr}`);
 //         return;
 //     }
-//     console.log(stdout);
+//     console.log(exec(`pgrep -f ${commandLine}`))
 // })
 
-let commandLine = "node grazyna.js"
-
-exec(commandLine, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(exec(`pgrep -f ${commandLine}`))
-})
